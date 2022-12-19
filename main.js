cfaaf55ecda4924f82219ef1e1e03355cec5a739
stellaris9950 <andyzihengwei@gmail.com>
2022-12-19 13:28:51 -0700

    fdfdf
    jajaja

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 console.log("hi");
 
+// Event listeners
+let addBtn = document.getElementById('addAgenda');
+addBtn.addEventListener('click', addAgenda);
+
+let Agenda = [];
 
 
+
+// Add new agenda
+function addAgenda() {
+  let planDay = +prompt("Enter Agenda Added Day:") - 1;
+  let planTime = prompt("Enter Time:");
+  let planDescription = prompt("Enter Description:");
+  Agenda[planDay] = newAgenda(planTime, planDescription);
+}
+function newAgenda(){
+  
+}
+
+function AgendaString(planTime, planDescription) {
+  return `
+    <span>${planTime}: ${planDescription}</sapn>
+  `;
+}
+
+
+
+// drawing the Days
 let outputEl = document.getElementById('output');
 displayDiv()
-function displayDiv() {
+function displayDiv(agenda) {
     let outputStr = '';
     for (let i = 0; i < 30; i++){
       outputStr += createDiv(i);
@@ -12,10 +38,22 @@ function displayDiv() {
     outputEl.innerHTML = outputStr;
 }
 function createDiv(i) {
-    return `
-      <div class="grid-item">
-        <span id="days">${i+1}</span>
+  return `
+    <div class="grid-item">
+      <span class="day-number">${i+1}</span>
+      ${Agenda[i]}
+    </div>
+  `
+}
 
-      </div>
-    `
+function saveAgenda() {
+  localStorage.setItem('Agenda', JSON.stringify(Agenda))
+}
+
+
+// load Contact from local storage
+
+function loadStorage() {
+  let AgendaStr = localStorage.getItem('Agenda');
+  return JSON.parse(AgendaStr) ?? [];
 }
